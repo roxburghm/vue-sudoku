7040a535152e67a1fe88e4b879bbb4cc9caf0e8b
roxburghm <matt@roxburghm.com>
2023-05-03 23:20:11 +0100

    pwa short name

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
         backgroundColor: "#ffffff",
 
         manifestOptions: {
-            shortName: "Sudoku",
+            short_name: "Sudoku",
             name: "Sudoku",
             display: "standalone",
             faviconSVG: null,
