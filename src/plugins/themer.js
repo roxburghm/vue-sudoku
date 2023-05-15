d8025b2707bee5a92894cbb6b37294e2d0338f7d
Matt Roxburgh <matt@roxburghm.com>
2023-05-15 19:41:20 +0100

    fix wanring clash on orange

--- src/plugins/themer.js
+++ src/plugins/themer.js
@@ -1,24 +1,29 @@
 const AVAILABLE_THEMES = {
     blue: SimpleTheme('#4dc2f8'),
-    orange: SimpleTheme('#e36d0e'),
+    orange: SimpleTheme('#e36d0e', '#e36d0e', '#7D1128'),
     purple: SimpleTheme('#b0789a'),
-    green: SimpleTheme('#A1CCA5')
+    green: SimpleTheme('#A1CCA5'),
+    beige: SimpleTheme('#8F7E4F'),
 }
 
-function SimpleTheme(color, darkColor) {
+function SimpleTheme(color, darkColor, warning) {
     if (darkColor === undefined) darkColor = color;
-    return {
+    if (warning === undefined) warning = '#fb8c00';
+    let theme = {
         light: {
             neutral: '#888',
             sudoku: color,
+            warning: warning,
             'sudoku-cell-color': '#fff',
         },
         dark: {
             neutral: '#555',
             sudoku: darkColor,
+            warning: warning,
             'sudoku-cell-color': '#222',
         }
     }
+    return theme;
 }
 
 function GetBaseColor(color) {
